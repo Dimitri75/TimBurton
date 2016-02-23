bdf28ff7a223690cba3da261d7a02aedd8f206c3
Dimitri75 <bdimitri75@gmail.com>
2016-02-23 15:00:04 -0500

    add view show_film displayed after clicking on a movie

--- views/show_film.php
+++ views/show_film.php
@@ -1,27 +1,26 @@
+<?php
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $query = "";
+
+        $db = connectDB();
+        $result = $db->query("SELECT * FROM `film` WHERE id='".$id."'");
+        $data = $result->fetch();
+    }
+?>
+
 <div id="main">
     <section>
-        <h3>Hello World !</h3>
+        <h3><?php echo $data['name']?></h3>
         <br/><br/>
 
         <figure class="large">
-            <img src="/timburton/resources/timburton.png"/>
-            <figcaption>Timothy Walter Button (1958)</figcaption>
+            <img src="/timburton/resources/film/<?php echo $data['illustration']?>"/>
+            <figcaption><?php echo $data['name']?> (<?php echo $data['date']?>)</figcaption>
         </figure>
 
-        <p>
-            Lorem ipsum dolor sit amet, fastidii ullamcorper vel ad. Autem torquatos vim no, dicam principes quo ne, ne pro discere commune persequeris. Mea mutat dictas ea, est feugiat dolores in. Eum suavitate forensibus scribentur et, at inimicus postulant his, eu graeci signiferumque has. Sit cu justo molestie, omnes denique eam at, docendi offendit voluptua an sea. Cu illud possit per, ei est movet fuisset concludaturque.
-        </p>
-        <p>
-            Cu est alterum maiorum molestie, assum alterum definitionem ea duo. In per suas perpetua, quot appellantur has an. Eu vix wisi quas. At mutat corpora eam. Utroque hendrerit definitionem te ius, brute philosophia ut sed.
-        </p>
-        <p>
-            Sit ne aperiri referrentur comprehensam, duo homero accusata tincidunt et, usu saepe partem disputando ea. Et iracundia vulputate est, ne splendide referrentur complectitur quo, sea detraxit iracundia definitionem cu. Modo affert lobortis sit ne. Ei vero tempor offendit sea, te mentitum recusabo quaerendum pro.
-        </p>
-        <p>
-            Ei eam convenire forensibus. Sea no quod dicit sadipscing, duo ei labore tractatos. Vero copiosae et eum. Ei vel iusto consetetur. Nullam tibique salutandi in his, viderer maluisset vituperatoribus eam ei.
-        </p>
-        <p>
-            Nullam volumus quo an, everti sensibus id quo. Eos an unum quodsi eripuit, diam sint assueverit in quo. Nam timeam comprehensam eu, agam virtute mea at, cu vel novum discere maiorum. Tantas nonumes perpetua duo et, integre petentium cu vix. Brute omnium numquam vix eu, etiam minimum nominati has at. Mel stet perfecto gubergren te, eos semper vivendo ad.
+        <p class="resume">
+            <?php echo $data['resume']?>
         </p>
     </section>
 </div>
