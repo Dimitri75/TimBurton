69170ba31d3c0ec8c2b8661c9f1faea2e0d37d67
Dimitri75 <bdimitri75@gmail.com>
2016-02-22 22:03:37 -0500

    delete views/base folder

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
     session_start();
 
     include_once "pdo.php";
-    include_once "views/base/header.html";
-    include_once "views/admin_userTable.php";
-    include_once "views/base/footer.html";
+    include_once "header.html";
+    include_once "views/admin_connection.php";
+    include_once "footer.html";
 ?>
 
 <script src="scripts/changeBackgroundOnScroll.js"></script>
