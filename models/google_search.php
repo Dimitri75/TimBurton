560d7de7db1b7076b3ed5dcbf7f76ddaef74179e
Kevin Suy <Kevin Suy>
2016-04-17 17:13:38 -0400

    Posters

--- models/google_search.php
+++ models/google_search.php
@@ -1,6 +1,6 @@
 <?php
 
-    function getMoviePoster($movieName){
+    function getMoviePosterOld($movieName){
         $query = "https://www.googleapis.com/customsearch/v1";
         $cx = "007358547476902121722:1vpylmrpooa";
         $apiKey = "AIzaSyA3qPMv3FRVifPhxteLskBzv5xzXaKUGbA";
@@ -10,4 +10,8 @@
         return $query;
     }
 
+    function getMoviePoster($movieName){
+        $query = "http://www.omdbapi.com/?t=" . urlencode($movieName) . "&y=&plot=short&r=json";
+        return $query;
+    }
 ?>
--- views/gallery.php
+++ views/gallery.php
@@ -31,21 +31,12 @@
                 <?php
                     foreach($movies["results"]["bindings"] as $data){
                         $label = removeStringInParentheses($data["label"]["value"]);
-//                        $tmp = getMoviePoster($data["label"]["value"]);
-//                        $searchResult = resultFromQueryForImages($tmp);
-//                        echo    "<li>
-//                                    <a href='/timburton/?action=show_film&id=".$data["wiki"]["value"]."'>
-//                                        <figure class='tiny'>
-//                                            <img src='".$searchResult->items[0]->pagemap->cse_image[0]->src."'/>
-//                                            <figcaption>".$data["label"]["value"]."</figcaption>
-//                                        </figure>
-//                                    </a>
-//                                </li>";
-
+                        $moviePosterQuery = getMoviePoster($label);
+                        $searchResult = resultFromQueryForImages($tmp);
                         echo    "<li>
                                     <a href='/timburton/?action=show_film&id=".$data["wiki"]["value"]."'>
                                         <figure class='tiny'>
-                                            <img src='#'/>
+                                            <img src='" . $searchResult->Poster . "'/>
                                             <figcaption>".$label."</figcaption>
                                         </figure>
                                     </a>
